cbbdb7bb303266c799bc97eb0eca67e22c7b27e5
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2023-10-28 10:46:25 +0700

    Setup Message Broker for services messaging

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection.Metadata.Ecma335;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,13 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
