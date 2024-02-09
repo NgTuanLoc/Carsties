d196be9de4dbc31de1e7f538ea9ac6fad44790d0
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-02-09 16:01:46 +0700

    refactoring useAuctionStore

--- frontend/app/auctions/Listings.tsx
+++ frontend/app/auctions/Listings.tsx
@@ -7,11 +7,19 @@ import Filters from './Filters'
 import { useParamsStore } from '../hooks/useParamsStore'
 import AppPagination from '../components/AppPagination'
 import { getData } from '../actions/auctionActions'
-import { Auction, PagedResult } from '@/types'
 import EmptyFilter from '../components/EmptyFilter'
+import { useAuctionStore } from '../hooks/useAuctionStore'
 
 const Listings = () => {
-  const [data, setData] = useState<PagedResult<Auction>>()
+  const [loading, setLoading] = useState(true)
+  const data = useAuctionStore(
+    useShallow((state) => ({
+      auctions: state.auctions,
+      totalCount: state.totalCount,
+      pageCount: state.pageCount,
+    })),
+  )
+  const setData = useAuctionStore((state) => state.setData)
   const params = useParamsStore(
     useShallow((state) => ({
       searchTerm: state.searchTerm,
@@ -33,12 +41,14 @@ const Listings = () => {
   useEffect(() => {
     getData(url).then((data) => {
       setData(data)
+      setLoading(false)
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url])
 
   if (!data) return <h3>Loading...</h3>
 
-  if (!data.results.length)
+  if (!data.auctions.length)
     return (
       <>
         <Filters />
@@ -50,7 +60,7 @@ const Listings = () => {
     <>
       <Filters />
       <div className='grid grid-cols-4 gap-6'>
-        {data.results.map((item) => (
+        {data.auctions.map((item) => (
           <AuctionCard key={item.id} auction={item} />
         ))}
       </div>
