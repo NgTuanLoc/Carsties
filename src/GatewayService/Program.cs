c551f9cfced87a68726d6fea4b6f3d00fc64105a
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-02-09 15:47:02 +0700

    add NotificationService and update dockerfile

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -13,10 +13,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+// Add Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
 
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
